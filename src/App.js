fc64b4c718bf256631e432e6624a11f3e8df59e5
faustabmnyh <faustaabimanyu@gmail.com>
2021-04-02 00:00:16 +0700

    fix bugv

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Signin from "./pages/Signin";
 import MainApp from "./pages/MainApp";
 import Register from "./pages/Register";
